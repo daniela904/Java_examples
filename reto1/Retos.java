d4cdd7e4925ceccfde73925698c0cea862b22370
daniela904 <87763778+daniela904@users.noreply.github.com>
2021-12-30 16:34:11 -0500

    Update Retos.java

--- reto1/Retos.java
+++ reto1/Retos.java
@@ -1,7 +1,17 @@
-package retos;
+/*La corporación ACME dedicada a la fabricación y distribución de productos peligrosos tiene como objetivo posicionarse en el comercio digital. Hace unos días lanzó su portal de ventas, pero los materiales peligrosos representan riesgos en su distribución, de manera que el equipo de marketing junto con el de producción en su análisis determinaron el índice de riesgo en la distribución de un producto (basado en aspectos como la materia prima, químicos, volumen, etc). Los valores del riesgo de distribución depende de tres factores representados como números enteros:
+FRD1:. Corresponde al índice inicial de riesgo de distribución definido por la Agencia de Distribución de Materiales Peligrosos ADMP. 
+FRD2: Corresponde a una relación de riesgo definida por el equipo ACME y que se basa en el valor FRD1 para su cálculo: FRD2 equivale a cuatro más el doble del valor del primer factor de costo (FRD1).
+FRD3: Corresponde al índice de riesgo de distribución en ACME. Cinco veces el valor del tercer factor de costo (FRD3) equivale a la suma de los valores obtenidos en el primer y segundo factor de costo, FRD1 y FRD2 respectivamente.
+El nivel de riesgo de distribución del producto adquirido por el cliente estará sujeto al valor obtenido del tercer factor de costo FRD3 así: 
+Si el valor de FRD3 está entre 0 y 20, la distribución tendrá riesgo 1. 
+Si el valor de FRD3 se encuentra entre 21 y 30 la distribución tendrá riesgo 2. 
+En caso de que el valor de FRD3 esté entre 31 y 50, su riesgo será 3. 
+Finalmente, si el valor de FRD3 es mayor a 51, el riesgo será 4.
+Elabora un programa en Java que permita, dado el factor de riesgo de distribución de un producto según la ADMP, conocer los valores obtenidos en los diferentes factores de riesgo en la distribución (FRD1, FRD2, FRD3), así como el nivel de riesgo de distribución para ACME en la que se encuentra el producto.*/
+
 import java.util.Scanner;
 
-public class Retos {
+public class reto1 {
     public static void main(String[] args) {
         int FRD1 = 0;
         int FRD2 = 0;
